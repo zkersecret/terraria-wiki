b19c2cbe4b7183e9c546e81cbbc60f055e399b63
fakersl <gustavosantosferreira00101@gmail.com>
2025-02-22 05:00:04 -0300

    Corrigindo

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // Importe o plugin do Tailwind CSS
 
 export default defineConfig(({ mode }) => {
   const base = mode === "production" ? "/terraria-wiki/" : "/";
   return {
-    plugins: [react()],
-    base,
+    plugins: [
+      react(),
+      tailwindcss(), // Adicione o plugin do Tailwind CSS
+    ],
+    base, // A propriedade `base` deve estar dentro do objeto retornado
   };
 });
