44a27e4da82c3d4bee64a1daa501cb644d6aeeef
fakersl <gustavosantosferreira00101@gmail.com>
2025-02-22 05:25:10 -0300

    Teste de commit

--- src/pages/Bosses.jsx
+++ src/pages/Bosses.jsx
@@ -1,8 +1,17 @@
+import bossData from "../data/bosses.json";
+
 export default function Bosses() {
-    return (
-      <div className="p-4">
-        <h1 className="text-2xl font-bold">Bosses</h1>
-        <p>Informações sobre todos os Bosses do jogo.</p>
-      </div>
-    );
-  }
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Bosses</h1>
+      <ul>
+        {bossData.map((boss) => (
+          <li key={boss.id} className="my-2">
+            <h2 className="text-xl font-semibold">{boss.name}</h2>
+            <p>{boss.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
